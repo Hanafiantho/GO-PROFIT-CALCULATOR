1b181a48b638dbe91cc7721cf346f6a42330f9d6
Hanafiantho <hanafiantho@gmail.com>
2025-10-16 10:11:24 +0700

    add error validation and store value into file

--- main.go
+++ main.go
@@ -1,30 +1,67 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 func main() {
 	// var revenue float64
 	// var expenses float64
 	// var taxRate float64
 
-	revenue := getUserInput("Revenue : ")
-	expenses := getUserInput("Expenses : ")
-	taxRate := getUserInput("taxRate : ")
+	revenue, err := getUserInput("Revenue : ")
+
+	if err != nil {
+		fmt.Println("Error")
+		fmt.Println(err)
+		fmt.Println("________")
+		return
+	}
+
+	expenses, err := getUserInput("Expenses : ")
+
+	if err != nil {
+		fmt.Println("Error")
+		fmt.Println(err)
+		fmt.Println("________")
+		return
+	}
+
+	taxRate, err := getUserInput("taxRate : ")
+
+	if err != nil {
+		fmt.Println("Error")
+		fmt.Println(err)
+		fmt.Println("________")
+		return
+	}
 
 	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
 
 	fmt.Printf("Earning before tax : %.1f \n", ebt)
 	fmt.Printf("Profit : %.1f \n", profit)
 	fmt.Printf("Ratio : %.3f \n", ratio)
+	storeResultIntoFile(ebt, profit, ratio)
 }
 
-func getUserInput(infoText string) float64 {
+func storeResultIntoFile(ebt, profit, ratio float64) {
+	result := fmt.Sprintf("EBT :%.1f\nProfit :%.1f\nRatio :%.3f", ebt, profit, ratio)
+	os.WriteFile("results.txt", []byte(result), 0644)
+}
+
+func getUserInput(infoText string) (float64, error) {
 	var userInput float64
 
 	fmt.Print(infoText)
 	fmt.Scan(&userInput)
 
-	return userInput
+	if userInput <= 0 {
+		return userInput, errors.New("value must be bigger than 0")
+	}
+
+	return userInput, nil
 }
 
 func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, float64) {
